e26a002322db563f14e27ed74c9e8d72cb93b145
CultLeaderZiad <cultleaderziad@gmail.com>
2026-08-22 23:02:26 +0300

    update-6

--- backend/app/models/creative.py
+++ backend/app/models/creative.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, ForeignKey, Float
+from sqlalchemy import Column, String, Integer, ForeignKey, Float, Boolean
 from sqlalchemy.orm import relationship
 from app.db.base import Base
 import uuid
